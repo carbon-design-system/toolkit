3d2054902151f687f4826e42f636aa1050cebf9d
Josh Black <joshblack@us.ibm.com>
2018-08-07 15:14:24 -0500

    feat(cli-plugin-paths): add paths plugin

---
+++ packages/cli-plugin-paths/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const url = require('url');
+
+// Inspired by Facebook's Create Reac App:
+// https://github.com/facebook/create-react-app/blob/next/packages/react-scripts/config/paths.js
+module.exports = ({ api, env, options }) => {
+  api.extend('project.paths', () => {
+    // Make sure any symlinks in the project folder are resolved:
+    // https://github.com/facebookincubator/create-react-app/issues/637
+    const appDirectory = fs.realpathSync(env.cwd);
+    const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+    const envPublicUrl = process.env.PUBLIC_URL;
+
+    function ensureSlash(path, needsSlash) {
+      const hasSlash = path.endsWith('/');
+      if (hasSlash && !needsSlash) {
+        return path.substr(path, path.length - 1);
+      } else if (!hasSlash && needsSlash) {
+        return `${path}/`;
+      } else {
+        return path;
+      }
+    }
+
+    const getPublicUrl = appPackageJson =>
+      // eslint-disable-next-line import/no-dynamic-require
+      envPublicUrl || require(appPackageJson).homepage;
+
+    // We use `PUBLIC_URL` environment variable or "homepage" field to infer
+    // "public path" at which the app is served.
+    // Webpack needs to know it to put the right <script> hrefs into HTML even in
+    // single-page apps that may serve index.html for nested URLs like /todos/42.
+    // We can't use a relative path in HTML because we don't want to load something
+    // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
+    function getServedPath(appPackageJson) {
+      const publicUrl = getPublicUrl(appPackageJson);
+      const servedUrl =
+        envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+      return ensureSlash(servedUrl, true);
+    }
+
+    return {
+      appBuild: resolveApp('build'),
+      appClient: resolveApp('src/client'),
+      appPublic: resolveApp('public'),
+      appIndexJs: resolveApp('src/client/index.js'),
+      appPackageJson: resolveApp('package.json'),
+      appServerIndexJs: resolveApp('src/server/index.js'),
+      appSrc: resolveApp('src'),
+      yarnLockFile: resolveApp('yarn.lock'),
+      appNodeModules: resolveApp('node_modules'),
+      publicUrl: getPublicUrl(resolveApp('package.json')),
+      servedPath: getServedPath(resolveApp('package.json')),
+      ...options,
+    };
+  });
+};
