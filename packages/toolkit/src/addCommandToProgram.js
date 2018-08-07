1f7a556d9a6a594160420e38033c3db419331e4c
Josh Black <joshblack@us.ibm.com>
2018-08-07 16:11:34 -0500

    feat(toolkit): add support for api commands in dev and prod

---
+++ packages/toolkit/src/addCommandToProgram.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const { logger } = require('@carbon/cli-tools');
+
+function addCommandToProgram(program, command, CLI_ENV) {
+  const { name, description, options = [], action, ...rest } = command;
+  const cliCommand = program.command(name).description(description);
+
+  for (let option of options) {
+    const { flags, description, defaults, development } = option;
+
+    if (development && CLI_ENV === 'development') {
+      const args = [flags, `${description} [DEV ONLY]`, defaults].filter(
+        Boolean
+      );
+      cliCommand.option(...args);
+    }
+  }
+
+  for (let key of Object.keys(rest)) {
+    if (cliCommand[key]) {
+      cliCommand[key]();
+    }
+  }
+
+  cliCommand.action(async (...args) => {
+    const commandArgs = args.slice(0, args.length - 1);
+    const command = args[args.length - 1];
+    const options = cleanArgs(command);
+
+    logger.trace(
+      'Running command:',
+      name,
+      'with args:',
+      commandArgs,
+      'and options:',
+      options
+    );
+
+    try {
+      await action(...commandArgs, options);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  });
+}
+
+// Inspired by Vue CLI:
+// https://github.com/vuejs/vue-cli/blob/31e1b4995edef3d2079da654deedffe002a1d689/packages/%40vue/cli/bin/vue.js#L172
+function cleanArgs(command) {
+  return command.options.reduce((acc, option) => {
+    // TODO: add case for reserved words from commander, like options
+
+    // Add case for mapping `--foo-bar` to `fooBar`
+    const key = option.long
+      .replace(/^--/, '')
+      .split('-')
+      .map((word, i) => {
+        if (i === 0) {
+          return word;
+        }
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join('');
+
+    // If an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof command[key] !== 'function') {
+      return {
+        ...acc,
+        [key]: command[key],
+      };
+    }
+    return acc;
+  }, {});
+}
+
+module.exports = addCommandToProgram;
--- packages/toolkit/src/index.js
+++ packages/toolkit/src/index.js
@@ -4,13 +4,14 @@ const { load } = require('@carbon/cli-runtime');
 const { logger } = require('@carbon/cli-tools');
 const program = require('commander');
 const packageJson = require('../package.json');
+const addCommandToProgram = require('./addCommandToProgram');
 
 const cwd = process.cwd();
 
 async function main({ argv }) {
   logger.trace(`Loading CLI for ${cwd}`);
 
-  const { error } = await load({ cwd });
+  const { error, api, env } = await load({ cwd });
   if (error) {
     throw error;
   }
@@ -20,6 +21,11 @@ async function main({ argv }) {
     .name('toolkit')
     .version(packageJson.version)
     .usage('<command> [options]');
+
+  for (const command of api.getCommands()) {
+    logger.trace(`Adding command ${command.name} to program`);
+    addCommandToProgram(program, command, env.CLI_ENV);
+  }
 
   program.parse(argv);
 }
