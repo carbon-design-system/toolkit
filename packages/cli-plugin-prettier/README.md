f787579d6187b3d5736d0cd588deccd4a201d795
Josh Black <joshblack@us.ibm.com>
2018-08-11 16:21:31 -0500

    feat(cli-plugin-prettier): add prettier plugin

---
+++ packages/cli-plugin-prettier/README.md
@@ -0,0 +1,21 @@
+# `@carbon/cli-plugin-prettier`
+
+> [`prettier`](https://prettier.io/) plugin for `@carbon/toolkit`
+
+## Getting Started
+
+Run the following command using [npm](https://www.npmjs.com/):
+
+```bash
+npm install -S @carbon/cli-plugin-prettier
+```
+
+If you prefer [Yarn](https://yarnpkg.com/en/), use the following command instead:
+
+```bash
+yarn add @carbon/cli-plugin-prettier
+```
+
+## 🤲 Contributing
+
+To learn more about how to contribute, look [here](/.github/CONTRIBUTING.md)!
---
+++ packages/cli-plugin-prettier/index.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const chalk = require('chalk');
+const { spawn } = require('@carbon/cli-tools');
+const npmWhich = require('npm-which');
+const util = require('util');
+
+const which = util.promisify(npmWhich(__dirname));
+const defaultOptions = {
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
+const defaultFileGlob = '**/*.{css,js,md,scss,ts}';
+
+module.exports = async ({ api, env, options }) => {
+  const prettier = await which('prettier');
+  const prettierOptions = {
+    ...defaultOptions,
+    ...options,
+  };
+
+  api.addCommand({
+    name: 'format',
+    description: 'format files in your project',
+    allowUnknownOption: true,
+    async action(cmd) {
+      const glob = typeof cmd === 'string' ? cmd : defaultFileGlob;
+      const args = [...formatOptions(prettierOptions), '--write', glob];
+
+      // eslint-disable-next-line no-console
+      console.log(chalk.grey(`prettier ${args.join(' ')}`));
+
+      await spawn(prettier, args, {
+        stdio: 'inherit',
+        cwd: env.cwd,
+      });
+    },
+  });
+
+  api.addCommand({
+    name: 'format:diff',
+    description: 'determine which files have not been formatted yet',
+    allowUnknownOption: true,
+    async action(cmd) {
+      const glob = typeof cmd === 'string' ? cmd : defaultFileGlob;
+      const args = [
+        ...formatOptions(prettierOptions),
+        '--list-different',
+        glob,
+      ];
+
+      // eslint-disable-next-line no-console
+      console.log(chalk.grey(`prettier ${args.join(' ')}`));
+
+      await spawn(prettier, args, {
+        stdio: 'inherit',
+        cwd: env.cwd,
+      });
+    },
+  });
+
+  api.add(async ({ extendPackageJson }) => {
+    await extendPackageJson(({ cliPath }) => ({
+      scripts: {
+        format: `${cliPath} format`,
+        'format:diff': `${cliPath} format:diff`,
+      },
+    }));
+  });
+};
+
+function splitCamelCase(string) {
+  const words = [''];
+  let wordPosition = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      words[++wordPosition] = '';
+    }
+    words[wordPosition] += string[i].toLowerCase();
+  }
+  return words;
+}
+
+function formatOptions(options) {
+  return Object.keys(options)
+    .map(key => {
+      const flag = splitCamelCase(key).join('-');
+      const value = options[key];
+
+      if (typeof value === 'boolean') {
+        return [`--${flag}`];
+      }
+
+      return [`--${flag}`, value];
+    })
+    .reduce((acc, flags) => acc.concat(flags), []);
+}
