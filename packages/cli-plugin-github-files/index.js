f6509b28219bc5cf848ca35aa6d4fd5d892a9675
Josh Black <joshblack@us.ibm.com>
2018-08-16 11:14:12 -0500

    feat(cli-plugin-github-files): add confirm prompt

--- packages/cli-plugin-github-files/index.js
+++ packages/cli-plugin-github-files/index.js
@@ -1,9 +1,31 @@
 'use strict';
 
+const fs = require('fs-extra');
 const path = require('path');
 
 const files = path.join(__dirname, 'files');
 
-module.exports = ({ api }) => {
-  api.add(({ copyFolder }) => copyFolder(files, '.github'));
+module.exports = ({ api, env, options }) => {
+  api.add(async ({ copyFolder }) => {
+    const folderPath = path.join(env.cwd, '.github');
+
+    if (!(await fs.pathExists(folderPath))) {
+      await copyFolder(files, '.github');
+      return;
+    }
+
+    const { confirm } = await api.prompt([
+      {
+        name: 'confirm',
+        message:
+          'Looks like there is a GitHub folder already at ' +
+          `${folderPath}.\nAre you sure you want to override it?`,
+        type: 'confirm',
+      },
+    ]);
+
+    if (confirm) {
+      await copyFolder(files, '.github');
+    }
+  });
 };
